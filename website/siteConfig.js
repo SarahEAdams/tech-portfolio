1a0c9acce8f01c143dce04a77b29fa455c0a2c3a
Sarah <sa4fa@virginia.edu>
2020-10-02 15:24:52 -0400

    Updating Tech Portfolio to match my personal information

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    title: 'Sarah Adams', // Title for your website.
+    tagline: 'Using data science as a tool to midigate societal gaps',
+    profilePhoto: 'img/profile.jpg',
+    url: 'https://SarahEAdams.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'SarahEAdams',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'sarah-adams-link',
+        twitter: 'sarah_datasci',
+        github: 'SarahEAdams',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
